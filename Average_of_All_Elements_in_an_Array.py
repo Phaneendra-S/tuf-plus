20fc59f02a23f8da0b4d2b3cf36aa3b450e596ff
Phaneendra-S <98729995+Phaneendra-S@users.noreply.github.com>
2025-10-09 00:01:54 +0530

    Create Average_of_All_Elements_in_an_Array.py

---
+++ Average_of_All_Elements_in_an_Array.py
@@ -0,0 +1,6 @@
+#Given an array nums of n integers, return Average of all Elements of given array.
+
+class Solution:
+    def averageOfArray(self, nums):
+        # Your code goes here
+        return sum(nums)/len(nums)
