c39de9167c3dc51e42ecb837bdc4871a4342b63b
Phaneendra-S <98729995+Phaneendra-S@users.noreply.github.com>
2025-10-18 21:36:38 +0530

    Create Concatenate_One_String_to_Another.py

---
+++ Concatenate_One_String_to_Another.py
@@ -0,0 +1,9 @@
+#You are given two strings str1 and str2.
+#Write a program to concatenate str2 to the end of str1, and return the resulting combined string.
+#Concatenation means joining both strings in their original order.
+
+
+class Solution:
+    def concatenate(self, str1: str, str2: str) -> str:
+        # Your code goes here
+        return str1+str2
