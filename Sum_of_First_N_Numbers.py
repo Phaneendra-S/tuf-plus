467404fefd5e7799db788b8a313a3afffaf3689c
Phaneendra-S <98729995+Phaneendra-S@users.noreply.github.com>
2025-11-14 23:57:06 +0530

    Create Sum_of_First_N_Numbers.py

---
+++ Sum_of_First_N_Numbers.py
@@ -0,0 +1,6 @@
+# Given an integer N, return the sum of first N natural numbers. Try to solve this using recursion.
+
+class Solution:
+    def NnumbersSum(self, N):
+        #your code goes here
+        return (N*(N+1))//2
